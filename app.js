af2a7b98bf48277a7d0d882738661bc75da2a355
vlad-tsvetkov <vlad-tsvetkov@yandex-team.ru>
2019-12-02 19:59:03 +0300

    session-id

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const usersRouter = require('./routes/users');
 const dishRouter = require('./routes/dishRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const leaderRouter = require('./routes/leaderRouter');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+
 
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
@@ -21,6 +24,15 @@ connect.then((db) => {
 
 const app = express();
 
+app.use(cookieParser('12345-67890-09876-54321'));
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -31,8 +43,29 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', usersRouter);
+app.use(auth);
+
+function auth (req, res, next) {
+  console.log(req.session);
+
+  if(!req.session.user) {
+    const err = new Error('You are not authenticated!');
+    err.status = 403;
+    return next(err);
+  }
+  else {
+    if (req.session.user === 'authenticated') {
+      next();
+    }
+    else {
+      const err = new Error('You are not authenticated!');
+      err.status = 403;
+      return next(err);
+    }
+  }
+}
+
 app.use('/dishes', dishRouter);
 app.use('/promotions', promotionRouter);
 app.use('/leaders', leaderRouter);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,69 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+
+const User = require('../models/users');
+
+router.use(bodyParser.json());
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/signup', (req, res, next) => {
+  User.create({
+    username: req.body.username,
+    password: req.body.password
+  }).then(user => {
+    res.json(user)
+  }).catch(error => next(error))
+});
+
+router.post('/login', (req, res, next) => {
+  if(!req.session.user) {
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader) {
+      res.statusCode = 401;
+      res.end('You are not logged in');
+    }
+
+    const auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    const username = auth[0];
+    const password = auth[1];
+
+    User.findOne({username: username})
+        .then((user) => {
+          if (user === null) {
+            const err = new Error('User ' + username + ' does not exist!');
+            err.status = 403;
+            return next(err);
+          }
+          else if (user.password !== password) {
+            const err = new Error('Your password is incorrect!');
+            err.status = 403;
+            return next(err);
+          }
+          else if (user.username === username && user.password === password) {
+            req.session.user = 'authenticated';
+            res.end('You are authenticated!')
+          }
+        })
+  }
+});
+
+router.get('/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy();
+    res.clearCookie('session-id');
+    res.redirect('/');
+  }
+  else {
+    const err = new Error('You are not logged in!');
+    err.status = 403;
+    next(err);
+  }
 });
 
 module.exports = router;
